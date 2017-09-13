fdf899f9c6cde0edbf65c049d21335afe9a17e95
Thanzex <Dell@XPS>
2017-09-13 17:11:58 +0200

    Opengl- bebgin test

--- gravity/gravity.cpp
+++ gravity/gravity.cpp
@@ -9,7 +9,7 @@
 #include "Force.h"
 #include "Planet.h"
 #include "Calcs.h"
-//#include "GLFW\glfw3.h"
+#include "GLFW\glfw3.h"
 using namespace std;
 
 const int N = 3;
@@ -17,7 +17,7 @@ const int N = 3;
 
 int main()
 {
-	/*
+	
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
@@ -36,25 +36,22 @@ int main()
 
 	Gluint vbo;
 	glGenBuffers(1, &vbo);
-	*/
 
 	int i = 0;
 	Planet a[N] = { Planet(Vector(1.0f, 1.0f, 1.0f), Vector(0.0f, 0.0f, 0.0f), 100.0f, 1.0f) ,\
 					Planet(Vector(100.0f, 100.0f, 0.0f), Vector(0.0f, 0.0f, 0.0f), 10.0f, 1.0f) ,\
 					Planet(Vector(100.0f, 0.0f, 0.0f), Vector(0.0f, 0.0f, 0.0f), 10.0f, 1.0f) };
-		while (true)//(!glfwWindowShouldClose(window))
+		while (!glfwWindowShouldClose(window))
 		{
-			/*
 			glfwSwapBuffers(window);
 			glfwPollEvents();
-			*/
 
 
-			///*
+			/*
 			cout << "\n  x: " << a[0].position.x << "  y: " << a[0].position.y << "  z: " << a[0].position.z;
 			cout << "\t Bx: " << a[1].position.x << "  y: " << a[1].position.y << "  z: " << a[1].position.z;
 			cout << "\t Cx: " << a[2].position.x << "  y: " << a[2].position.y << "  z: " << a[2].position.z;
-			//*/
+			*/
 			for (i = 0; i < N; i++)
 			{
 				for (int j = 0; j < N; j++)
@@ -67,6 +64,6 @@ int main()
 				a[i].Update();
 			}		//Update
 		}
-		//glfwTerminate();
+		glfwTerminate();
 
 }
