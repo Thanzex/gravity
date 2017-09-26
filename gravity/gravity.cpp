87eb4ae039341306870c3474bd9bd283fa6996ef
Thanzex <Dell@XPS>
2017-09-26 18:03:31 +0200

    triangle working

--- gravity/gravity.cpp
+++ gravity/gravity.cpp
@@ -40,9 +40,9 @@ int main()
 	glBindVertexArray(vao);
 
 	float vertices[] = {
-		0.0f,0.5f,
-		0.5f,-0.5f,
-		-0.5f,-0.5f
+		0.0f,0.5f,	1.0f,0.0f,0.0f,
+		0.5f,-0.5f,	0.0f,1.0f,0.0f,
+		-0.5f,-0.5f,0.0f,0.0f,1.0f
 	};
 	// VBO CREATION
 
@@ -55,19 +55,23 @@ int main()
 	const char* vertexSource = R"glsl(
 		#version 150 core
 		in vec2 position;
+		in vec3 color;
+		out vec3 Color;
 
 		void main()
 		{
+			Color=color;
 			gl_Position = vec4(position, 0.0, 1.0);
 		})glsl";
 
 
 	const GLchar* fragmentSource = R"glsl(
 		#version 150 core
+		in vec3 Color;
 		out vec4 outColor;
 		void main()
 		{
-			outColor = vec4(1.0, 1.0, 1.0, 1.0);
+			outColor = vec4(Color, 1.0);
 		}
 	)glsl";
 
@@ -78,13 +82,14 @@ int main()
 
 	GLint status;
 	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &status);
-	if (status != GL_TRUE) cout << "Errore compilazione shader";
-	 //fragmentshader
+	if (status != GL_TRUE) cout << "Errore compilazione shader 1";
+	//fragmentshader
 	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
 	glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
 	glCompileShader(fragmentShader);
+
 	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &status);
-	if (status != GL_TRUE) cout << "Errore compilazione shader";
+	if (status != GL_TRUE) cout << "Errore compilazione shader 2";
 
 	//combine the two in a programshader
 	GLuint shaderProgram = glCreateProgram();
@@ -98,8 +103,11 @@ int main()
 
 	//Specify how data is to be interpreted
 	GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
-	glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 0, 0);
 	glEnableVertexAttribArray(posAttrib);
+	glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 5*sizeof(float), 0);
+	GLint colAttrib = glGetAttribLocation(shaderProgram, "color");
+	glEnableVertexAttribArray(colAttrib);
+	glVertexAttribPointer(colAttrib, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(2 * sizeof(float)));
 
 
 
@@ -108,34 +116,34 @@ int main()
 	Planet a[N] = { Planet(Vector(1.0f, 1.0f, 1.0f), Vector(0.0f, 0.0f, 0.0f), 100.0f, 1.0f) ,\
 					Planet(Vector(100.0f, 100.0f, 0.0f), Vector(0.0f, 0.0f, 0.0f), 10.0f, 1.0f) ,\
 					Planet(Vector(100.0f, 0.0f, 0.0f), Vector(0.0f, 0.0f, 0.0f), 10.0f, 1.0f) };
-		while (!glfwWindowShouldClose(window))
+	while (!glfwWindowShouldClose(window))
+	{
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+
+		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+
+		/*
+		cout << "\n  x: " << a[0].position.x << "  y: " << a[0].position.y << "  z: " << a[0].position.z;
+		cout << "\t Bx: " << a[1].position.x << "  y: " << a[1].position.y << "  z: " << a[1].position.z;
+		cout << "\t Cx: " << a[2].position.x << "  y: " << a[2].position.y << "  z: " << a[2].position.z;
+
+		for (i = 0; i < N; i++)
 		{
-			glDrawArrays(GL_TRIANGLES, 0, 3);
-			glfwSwapBuffers(window);
-			glfwPollEvents();
-
-			glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-			glClear(GL_COLOR_BUFFER_BIT);
-
-
-			/*
-			cout << "\n  x: " << a[0].position.x << "  y: " << a[0].position.y << "  z: " << a[0].position.z;
-			cout << "\t Bx: " << a[1].position.x << "  y: " << a[1].position.y << "  z: " << a[1].position.z;
-			cout << "\t Cx: " << a[2].position.x << "  y: " << a[2].position.y << "  z: " << a[2].position.z;
-			
-			for (i = 0; i < N; i++)
+			for (int j = 0; j < N; j++)
 			{
-				for (int j = 0; j < N; j++)
-				{
-					if (j != i) a[i].Addforce(Gravity(a[i], a[j]));
-				}
-			}	   //Gravity
-			for (i = 0; i < N; i++)
-			{
-				a[i].Update();
-			}		//Update
-			*/
-		}
-		glfwTerminate();
+				if (j != i) a[i].Addforce(Gravity(a[i], a[j]));
+			}
+		}	   //Gravity
+		for (i = 0; i < N; i++)
+		{
+			a[i].Update();
+		}		//Update
+		*/
+	}
+	glfwTerminate();
 
 }
